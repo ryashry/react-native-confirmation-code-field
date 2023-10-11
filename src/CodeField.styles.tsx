56d8da31573acc729a888fc2a4187dbe81ce5ed2
David <4661784+retyui@users.noreply.github.com>
2023-10-11 15:25:20 +0200

    Fix disabled  field by default on iOS after enabling the new architecture
    - Issue: https://github.com/retyui/react-native-confirmation-code-field/issues/230

--- src/CodeField.styles.tsx
+++ src/CodeField.styles.tsx
@@ -9,7 +9,7 @@ export const styles = StyleSheet.create({
     // <TextInput/> will be rendered above Cells
     ...StyleSheet.absoluteFillObject,
     // Hide <TextInput/> to simulate that user will press into a cell
-    opacity: 0.01,
+    opacity: 0.015,
     // Each user press into <TextInput/> should always set the cursor on the end of a text value
     fontSize: 1,
     ...Platform.select({
